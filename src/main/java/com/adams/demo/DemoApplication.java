c7c0128a45cfc154c6551db985df74df8e6f06f7
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-09-20 17:45:32 +0200

    Mostrar nombres y edades de un arreglo

--- src/main/java/com/adams/demo/DemoApplication.java
+++ src/main/java/com/adams/demo/DemoApplication.java
@@ -8,10 +8,18 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-        Persona pepito = new Persona ("Pepito", 40);
+        Persona[] personas = new Persona[4];
+        personas[0] = new Persona ("Pepito", 40);
+        personas[1] = new Persona ("Rosa", 32);
+        personas[2] = new Persona ("Roberto", 48);
+        personas[3] = new Persona ("Silvia", 52);
 
-        System.out.println("Hola "+ pepito.getNombre());
-        System.out.println("Edad: "+ pepito.getEdad());
+        for (int i = 0; i < personas.length; i++) {
+            System.out.println("Hola "+ personas[i].getNombre());
+            System.out.println("Edad: "+ personas[i].getEdad());
+        }
+
+
 
         //SpringApplication.run(DemoApplication.class, args);
 
